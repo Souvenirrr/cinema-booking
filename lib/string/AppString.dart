a2242cefa8737df7f818b5407a743cad5367d528
mduccc <duc2521997@gmail.com>
2019-12-10 10:02:42 +0700

    Modify AppString.dart

--- lib/string/AppString.dart
+++ lib/string/AppString.dart
@@ -1,16 +1,16 @@
 import 'package:intl/intl.dart';
 
 class AppString {
-  static final String thanhtoan = 'Thanh toán';
+  static final String pay = 'Thanh toán';
   static final String point = 'Point';
-  static final String ghe = 'ghế';
+  static final String movieChair = 'ghế';
   static final String vnd = 'VND';
-  static final String sodu = 'Số dư';
-  static final String manhinh = 'Màn hình';
-  static final String chongoi = 'Chỗ ngồi';
-  static final String saitaikhoan = 'Tài khoản không tồn tại';
-  static final String loiluutoken = 'Không thể lưu token';
-  static final String loiconnect = 'Lỗi kết nối';
+  static final String walletMoney = 'Số dư';
+  static final String bigScreen = 'Màn hình';
+  static final String cost = 'Chỗ ngồi';
+  static final String accountNotExist = 'Tài khoản không tồn tại';
+  static final String saveTokenError = 'Không thể lưu token';
+  static final String connectError = 'Lỗi kết nối';
   static final String phone = 'Số điện thoại';
   static final String email = 'Email';
   static final String male = 'Nam';
@@ -18,20 +18,22 @@ class AppString {
   static final String movieDetail = 'Chi tiết phim';
   static final String userInfo = 'Thông tin cá nhân';
   static final String logout = 'Đăng xuất';
-  static final String khongdusodu = 'Không đủ số dư để thanh toán';
-  static final String xacnhanthanhtoan = 'Xác nhận thanh toán';
+  static final String notEnoughMoney = 'Không đủ số dư để thanh toán';
+  static final String payConfirm = 'Xác nhận thanh toán';
   static final String someWrong = 'Có lỗi xảy ra';
+  static final String pleaseFitForm = 'Vui lòng điền đầy đủ';
+  static final String passwordIsNotMatch = 'Mật khẩu không khớp';
 
-  static String muave(
-    String tien,
-    int soluong,
+  static String dialogTicket(
+    String payMoney,
+    int ticketLength,
   ) =>
-      'Bạn sẽ thanh toán $tien ' +
+      'Bạn sẽ thanh toán $payMoney ' +
       AppString.vnd +
-      ' cho $soluong vé xem phim tại VGV';
-  static final String huy = 'Huỷ';
-  static final String thanhtoanthanhcong = 'Thanh toán thành công';
-  static final String vuilongdoi = 'Đợi xíu';
+      ' cho $ticketLength vé xem phim tại VGV';
+  static final String cancel = 'Huỷ';
+  static final String paySuccess = 'Thanh toán thành công';
+  static final String pleaseWait = 'Đợi xíu';
   static final String hintUsername = 'Tên đăng nhập';
   static final String hintPassword = 'Mật khẩu';
   static final String rePassword = 'Nhập lại mật khẩu';
